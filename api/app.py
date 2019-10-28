110f1922898658e4d91fa553b756d3adec842885
Ivan Ferro <iaferro@gmail.com>
2019-10-28 17:15:53 -0300

    api

---
+++ api/app.py
@@ -0,0 +1,18 @@
+
+import flask
+from controllers.users import blueprint as users
+## sempre deixar duas linhas em branco entre as import e o codigo
+
+app = flask.Flask(__name__)
+
+app.register_blueprint(users,url_prefix='/users')
+
+@app.route('/hello-word', methods=['GET'])
+def get_hello_word():
+    return flask.jsonify({
+        'messsage':'hello, world.'
+    })
+
+## preferivel usar ' do que "
+if __name__ == '__main__':
+    app.run()
---
+++ api/controllers/users.py
@@ -0,0 +1,46 @@
+
+import flask
+
+blueprint =  flask.Blueprint('users',__name__)
+
+@blueprint.route('/', methods=['GET'])
+def get_users():
+    return 'get all users'
+
+@blueprint.route('/', methods=['POST'])
+def post_users():
+    return 'post users'
+       
+@blueprint.route('/<userid>', methods=['GET'])
+def get_users_by_id(userid):
+    return f'get all users {userid}'
+
+
+@blueprint.route('/<userid>', methods=['DELETE'])
+def delete_users(userid):
+    return f'delete user {userid}'
+
+
+@blueprint.route('/<userid>', methods=['PUT'])
+def put_users(userid):
+    return f'put user {userid}'
+
+
+
+@blueprint.route('/<userid>', methods=['PATCH'])
+def patch_users(userid):
+    return f'patch user {userid}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
